f924a6388f0d22b3f241516ece7313dda5b8b065
nenw <khi@nenw.dev>
2025-01-20 15:26:35 +0900

    chore: repository type from implementation

--- src/bridge/types.ts
+++ src/bridge/types.ts
@@ -1,12 +1,6 @@
+import type { repositoryImpl } from './implementations/repository';
 import type { Config } from '@/config/schema';
 import type { RepositoryProxyDescriptor } from '@/utils/repositoryProxy';
-import type AstalHyprland from 'gi://AstalHyprland';
-import type AstalMpris from 'gi://AstalMpris';
-import type AstalNetwork from 'gi://AstalNetwork';
-import type AstalNotifd from 'gi://AstalNotifd';
-import type AstalTray from 'gi://AstalTray';
-import type AstalWp from 'gi://AstalWp';
-import type GTop from 'gi://GTop';
 
 /* Definitions */
 export type InitParams = (InitParamsDock | InitParamsStatusBar) & { config: Config };
@@ -21,14 +15,7 @@ export type BridgeMethodsRenderer = {
   initParams: (params: void) => InitParams;
   subscribe: (params: { descriptor: RepositoryProxyDescriptor }) => { id: string; value: unknown };
   unsubscribe: (params: { id: string }) => void;
+  debug: (params: unknown) => void;
 };
 
-export type BridgeRepository = {
-  hyprland: AstalHyprland.Hyprland;
-  musicPlayer: AstalMpris.Mpris;
-  network: AstalNetwork.Network;
-  notification: AstalNotifd.Notifd;
-  performance: GTop.glibtop;
-  sound: AstalWp.Wp;
-  tray: AstalTray.Tray;
-};
+export type BridgeRepository = typeof repositoryImpl;
