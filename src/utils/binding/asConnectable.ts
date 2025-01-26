34cf3dfdd8e2afcdace7c8170046d7e9d82f4b36
nenw <khi@nenw.dev>
2025-01-26 23:58:29 +0900

    feat: add set method in asConnectable

--- src/utils/binding/asConnectable.ts
+++ src/utils/binding/asConnectable.ts
@@ -1,11 +1,21 @@
 import { isObject } from '../type';
+import type { Variable } from 'astal';
 import type { Connectable, Subscribable } from 'astal/binding';
 
 const isSubscribable = (value: unknown): value is Subscribable =>
   isObject(value) && 'subscribe' in value && typeof value.subscribe === 'function';
 
-type Connectablized<T extends object> = Pick<Connectable, 'connect' | 'disconnect'> & {
-  readonly [K in keyof T]: T[K] extends Subscribable<infer TInner> ? TInner : T[K];
+const isVariable = (value: unknown): value is Variable<unknown> =>
+  isSubscribable(value) && 'set' in value && typeof value.set === 'function';
+
+type RemoveSubscribable<T> = T extends Subscribable<infer TInner> ? TInner : T;
+
+type ConnectablizedController<T extends object> = Pick<Connectable, 'connect' | 'disconnect'> & {
+  set(key: keyof T, value: RemoveSubscribable<T[keyof T]>): void;
+};
+
+type Connectablized<T extends object> = ConnectablizedController<T> & {
+  readonly [K in keyof T]: RemoveSubscribable<T[K]>;
 };
 
 export const asConnectable = <const T extends object>(object: T): Simplify<Connectablized<T>> => {
@@ -14,7 +24,7 @@ export const asConnectable = <const T extends object>(object: T): Simplify<Conne
   const subscriptionById = new Map<number, [string, (value: unknown) => void]>();
   const subscriptions = new Map<string, Set<(value: unknown) => void>>();
 
-  const controller: Connectable = {
+  const controller: ConnectablizedController<T> = {
     connect(signal, onUpdate) {
       const key = signal.slice(8).replace(/-[a-z]/g, match => match[1].toUpperCase());
       const value: unknown = object[key as keyof T];
@@ -58,12 +68,18 @@ export const asConnectable = <const T extends object>(object: T): Simplify<Conne
         subscriptions.delete(key);
       }
     },
+
+    set(id, value) {
+      if (isVariable(object[id])) {
+        object[id].set(value);
+      }
+    },
   };
 
   return new Proxy<Connectablized<T>>(object as Connectablized<T>, {
     get(target, prop, receiver: unknown) {
       if (Object.hasOwn(controller, prop)) {
-        return controller[prop as keyof Connectable] as unknown;
+        return controller[prop as keyof ConnectablizedController<T>] as unknown;
       }
 
       const value = Reflect.get(target, prop, receiver) as unknown;
