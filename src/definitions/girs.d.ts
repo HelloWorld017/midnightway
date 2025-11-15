0cc0eb890cb1c517211c5ffc067e0b94afddbdf3
nenw <khi@nenw.dev>
2025-11-15 12:30:45 +0900

    fix: fix typecheck failures

---
+++ src/definitions/girs.d.ts
@@ -0,0 +1,13 @@
+// Disable errors for unnecessary girs.
+
+declare module 'gi://AstalApps' {
+  export default any;
+}
+
+declare module 'gi://AstalBluetooth' {
+  export default any;
+}
+
+declare module 'gi://AstalPowerProfiles' {
+  export default any;
+}
